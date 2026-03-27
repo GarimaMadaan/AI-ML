ec99e70a676c3f743dbea868dcac24daeea273d9
Garima Madaan <135014389+GarimaMadaan@users.noreply.github.com>
2026-03-27 15:21:47 +0530

    Python Fundamentals

---
+++ FirstProgram/dataType.py
@@ -0,0 +1,10 @@
+name = "Garima"
+age = 22
+pie = 3.14
+isPrime = True
+isNotPrime = None
+print(type(name))
+print(type(age))
+print(type(pie))
+print(type(isPrime))
+print(type(isNotPrime))
---
+++ FirstProgram/variables.py
@@ -0,0 +1,6 @@
+name = "Garima"
+age = 22
+pie = 3.14
+print("My name is:", name)
+print("My age is:", age-5)
+print("The value of pi is:", pie)
