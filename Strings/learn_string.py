ea2f4870168722d23b5ff3a30f588fd9c57637f2
Garima Madaan <135014389+GarimaMadaan@users.noreply.github.com>
2026-04-06 17:35:17 +0530

    Strings code added

---
+++ Strings/learn_string.py
@@ -0,0 +1,10 @@
+word1 = "I love"
+word2 = "Python"
+
+# Concatenating the two strings
+result = word1 + " " + word2
+print(result)
+
+print("Length of the result string:", len(result))
+
+print(word1[2:5])
